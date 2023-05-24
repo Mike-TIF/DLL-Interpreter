3f2e57888436ebeeeab13ff03a96766ac61fab0a
Mike-TIF <mikescriven40@gmail.com>
2023-05-25 00:42:02 +0200

    Finished Up All Functionality For V1.0

--- DLL-Interpreter/BaseForm.Designer.cs
+++ DLL-Interpreter/BaseForm.Designer.cs
@@ -28,19 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node2");
+            this.SelectDllBtn = new System.Windows.Forms.Button();
+            this.DllPathLbl = new System.Windows.Forms.Label();
+            this.DllOutputTreeView = new System.Windows.Forms.TreeView();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.ParametersInputTxt = new System.Windows.Forms.TextBox();
+            this.TestInputOutputBtn = new System.Windows.Forms.Button();
+            this.OutputTxt = new System.Windows.Forms.RichTextBox();
+            this.OutputHeader = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectedMethodLbl = new System.Windows.Forms.Label();
+            this.SelectedTypeNameLbl = new System.Windows.Forms.Label();
+            this.SelectedParametersLbl = new System.Windows.Forms.Label();
+            this.OutputFieldLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // SelectDllBtn
+            // 
+            this.SelectDllBtn.Location = new System.Drawing.Point(498, 5);
+            this.SelectDllBtn.Name = "SelectDllBtn";
+            this.SelectDllBtn.Size = new System.Drawing.Size(100, 23);
+            this.SelectDllBtn.TabIndex = 0;
+            this.SelectDllBtn.Text = "SELECT DLL";
+            this.SelectDllBtn.UseVisualStyleBackColor = true;
+            this.SelectDllBtn.Click += new System.EventHandler(this.SelectDllBtn_Click);
+            // 
+            // DllPathLbl
+            // 
+            this.DllPathLbl.AutoEllipsis = true;
+            this.DllPathLbl.Location = new System.Drawing.Point(67, 11);
+            this.DllPathLbl.Name = "DllPathLbl";
+            this.DllPathLbl.Size = new System.Drawing.Size(425, 15);
+            this.DllPathLbl.TabIndex = 1;
+            this.DllPathLbl.Text = "label1";
+            // 
+            // DllOutputTreeView
+            // 
+            this.DllOutputTreeView.Location = new System.Drawing.Point(12, 34);
+            this.DllOutputTreeView.Name = "DllOutputTreeView";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Node0";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "Node4";
+            treeNode3.Name = "Node3";
+            treeNode3.Text = "Node3";
+            treeNode4.Name = "Node1";
+            treeNode4.Text = "Node1";
+            treeNode5.Name = "Node2";
+            treeNode5.Text = "Node2";
+            this.DllOutputTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode4,
+            treeNode5});
+            this.DllOutputTreeView.Size = new System.Drawing.Size(586, 387);
+            this.DllOutputTreeView.TabIndex = 2;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label3.Location = new System.Drawing.Point(12, 435);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(91, 21);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "Type Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 464);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Method Name:";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label4.Location = new System.Drawing.Point(279, 435);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(91, 21);
+            this.Label4.TabIndex = 5;
+            this.Label4.Text = "Parameters:";
+            // 
+            // ParametersInputTxt
+            // 
+            this.ParametersInputTxt.Location = new System.Drawing.Point(364, 462);
+            this.ParametersInputTxt.Name = "ParametersInputTxt";
+            this.ParametersInputTxt.Size = new System.Drawing.Size(234, 23);
+            this.ParametersInputTxt.TabIndex = 6;
+            // 
+            // TestInputOutputBtn
+            // 
+            this.TestInputOutputBtn.Location = new System.Drawing.Point(12, 491);
+            this.TestInputOutputBtn.Name = "TestInputOutputBtn";
+            this.TestInputOutputBtn.Size = new System.Drawing.Size(586, 23);
+            this.TestInputOutputBtn.TabIndex = 7;
+            this.TestInputOutputBtn.Text = "Test";
+            this.TestInputOutputBtn.UseVisualStyleBackColor = true;
+            this.TestInputOutputBtn.Click += new System.EventHandler(this.TestInputOutputBtn_Click);
+            // 
+            // OutputTxt
+            // 
+            this.OutputTxt.Location = new System.Drawing.Point(628, 34);
+            this.OutputTxt.Name = "OutputTxt";
+            this.OutputTxt.Size = new System.Drawing.Size(348, 502);
+            this.OutputTxt.TabIndex = 8;
+            this.OutputTxt.Text = "";
+            // 
+            // OutputHeader
+            // 
+            this.OutputHeader.AutoSize = true;
+            this.OutputHeader.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OutputHeader.Location = new System.Drawing.Point(628, 5);
+            this.OutputHeader.Name = "OutputHeader";
+            this.OutputHeader.Size = new System.Drawing.Size(62, 21);
+            this.OutputHeader.TabIndex = 9;
+            this.OutputHeader.Text = "Output:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Input:";
+            // 
+            // SelectedMethodLbl
+            // 
+            this.SelectedMethodLbl.AutoEllipsis = true;
+            this.SelectedMethodLbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SelectedMethodLbl.Location = new System.Drawing.Point(123, 464);
+            this.SelectedMethodLbl.Name = "SelectedMethodLbl";
+            this.SelectedMethodLbl.Size = new System.Drawing.Size(235, 21);
+            this.SelectedMethodLbl.TabIndex = 11;
+            // 
+            // SelectedTypeNameLbl
+            // 
+            this.SelectedTypeNameLbl.AutoEllipsis = true;
+            this.SelectedTypeNameLbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SelectedTypeNameLbl.Location = new System.Drawing.Point(98, 435);
+            this.SelectedTypeNameLbl.Name = "SelectedTypeNameLbl";
+            this.SelectedTypeNameLbl.Size = new System.Drawing.Size(184, 21);
+            this.SelectedTypeNameLbl.TabIndex = 12;
+            // 
+            // SelectedParametersLbl
+            // 
+            this.SelectedParametersLbl.AutoEllipsis = true;
+            this.SelectedParametersLbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SelectedParametersLbl.Location = new System.Drawing.Point(364, 438);
+            this.SelectedParametersLbl.Name = "SelectedParametersLbl";
+            this.SelectedParametersLbl.Size = new System.Drawing.Size(234, 21);
+            this.SelectedParametersLbl.TabIndex = 13;
+            // 
+            // OutputFieldLbl
+            // 
+            this.OutputFieldLbl.AutoEllipsis = true;
+            this.OutputFieldLbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.OutputFieldLbl.ForeColor = System.Drawing.Color.IndianRed;
+            this.OutputFieldLbl.Location = new System.Drawing.Point(12, 518);
+            this.OutputFieldLbl.Name = "OutputFieldLbl";
+            this.OutputFieldLbl.Size = new System.Drawing.Size(586, 21);
+            this.OutputFieldLbl.TabIndex = 14;
+            this.OutputFieldLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // BaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 436);
+            this.ClientSize = new System.Drawing.Size(995, 548);
+            this.Controls.Add(this.OutputFieldLbl);
+            this.Controls.Add(this.SelectedParametersLbl);
+            this.Controls.Add(this.SelectedTypeNameLbl);
+            this.Controls.Add(this.SelectedMethodLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputHeader);
+            this.Controls.Add(this.OutputTxt);
+            this.Controls.Add(this.TestInputOutputBtn);
+            this.Controls.Add(this.ParametersInputTxt);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.DllOutputTreeView);
+            this.Controls.Add(this.DllPathLbl);
+            this.Controls.Add(this.SelectDllBtn);
             this.Name = "BaseForm";
             this.Text = ".dll Interpreter v1.0";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button SelectDllBtn;
+        private Label DllPathLbl;
+        private TreeView DllOutputTreeView;
+        private Label Label3;
+        private Label label2;
+        private Label Label4;
+        private TextBox ParametersInputTxt;
+        private Button TestInputOutputBtn;
+        private RichTextBox OutputTxt;
+        private Label OutputHeader;
+        private Label label1;
+        private Label SelectedMethodLbl;
+        private Label SelectedTypeNameLbl;
+        private Label SelectedParametersLbl;
+        private Label OutputFieldLbl;
     }
 }
--- DLL-Interpreter/BaseForm.cs
+++ DLL-Interpreter/BaseForm.cs
@@ -1,10 +1,339 @@
+using System.Reflection;
+using System.Windows.Forms.VisualStyles;
+
 namespace DLL_Interpreter
 {
     public partial class BaseForm : Form
     {
+        string CurrentDllFile = "";
+        List<TypeInfo> CurrentDllTypeInfo = new List<TypeInfo>();
+        TreeNodeInfo CurrentTreeNodeInfo;
+
+        string TestFile = @"C:\Users\magma\OneDrive\Documents\DLL-Interpreter-TestLib\TestDll\bin\Debug\net6.0\TestDll.dll";
+
         public BaseForm()
         {
             InitializeComponent();
+            DllOutputTreeView.AfterSelect += OnTreeviewNodeSelected;
         }
+        private void SelectDllBtn_Click(object sender, EventArgs e)
+        {
+            //Open A OpenFileDialog window
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.Filter = "dll | *.dll";
+            ofd.InitialDirectory = @"C:\";
+
+            ofd.Title = "Select a .dll File:";
+
+            //PRODUCTION
+            //Return if cancelled
+            //if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            ////Save the info of the file selected
+            //CurrentDllFile = ofd.FileName;
+            //DllPathLbl.Text = CurrentDllFile;
+
+            //TESTING
+            CurrentDllFile = TestFile;
+            DllPathLbl.Text = CurrentDllFile;
+
+            //Shows output to the screen
+            DisplayAssemblyInfo();
+        }
+        private void OnTreeviewNodeSelected(object? sender, TreeViewEventArgs e)
+        {
+            AssemblyInterpreter ai = new();
+            CurrentTreeNodeInfo = ai.GetSelectedNodeInfo(DllOutputTreeView.SelectedNode);
+
+            SelectedTypeNameLbl.Text = CurrentTreeNodeInfo.TypeName;
+            SelectedMethodLbl.Text = CurrentTreeNodeInfo.MethodName;
+            SelectedParametersLbl.Text = CurrentTreeNodeInfo.Parameters;
+        }
+        void DisplayAssemblyInfo()
+        {
+            DllOutputTreeView.Nodes.Clear();
+            
+            AssemblyInterpreter ai = new AssemblyInterpreter();
+
+            CurrentDllTypeInfo = ai.GetDllInfo(CurrentDllFile).ToList();
+
+            //Looping through and displaying the types in the .dll
+            for (int TypeIndex = 0; TypeIndex < CurrentDllTypeInfo.Count(); TypeIndex++)
+            {
+                var dllType = CurrentDllTypeInfo.ElementAt(TypeIndex);
+                DllOutputTreeView
+                    .Nodes
+                    .Add(AssemblyInterpreter.TypeIdentifier + dllType.GetType().Name);
+
+                //Looping through and displaying the method names and return types in each Type
+                for (int MethodIndex = 0; MethodIndex < dllType.GetMethodInfo().Count(); MethodIndex++)
+                {
+                    var method = dllType.GetMethodInfo().ElementAt(MethodIndex);
+                    DllOutputTreeView
+                        .Nodes[TypeIndex]
+                        .Nodes
+                        .Add($"{AssemblyInterpreter.MethodIdentifier}{method.Name}:{method.ReturnType}");
+
+                    //Looping through and displaying all the parameters for each method
+                    for (int ParameterIndex = 0; ParameterIndex < method.ParameterInfo.Length; ParameterIndex++)
+                    {
+                        var parameter = method.ParameterInfo.ElementAt(ParameterIndex);
+                        DllOutputTreeView.Nodes[TypeIndex]
+                            .Nodes[MethodIndex]
+                            .Nodes
+                            .Add($"{AssemblyInterpreter.ParameterIdentifier}{parameter.Name}:{parameter.ParameterType.Name}" +
+                            //NOTE: if parameter.IsOptional it means the parameter is REQUIRED -- Not Sure Why, Bug?
+                            $" {(!parameter.IsOptional? "" : $"={parameter.DefaultValue}")}");
+                    }
+                }
+            }
+
+            DllOutputTreeView.Refresh();
+        }
+        private void TestInputOutputBtn_Click(object sender, EventArgs e)
+        {
+            if (CurrentTreeNodeInfo.Category != TypeCategory.METHOD)
+            {
+                //Show output?
+                OutputFieldLbl.Text = "Please select a Method before testing! " + CurrentTreeNodeInfo.Category;
+                return;
+            }
+
+            AssemblyInterpreter ai = new();
+            (TypeInfo type, DllMethodInfo mi) = ai.FindSelectedMethod(DllOutputTreeView.SelectedNode, CurrentDllTypeInfo);
+            object[] parameters = ai.GetParameterValues(mi, ParametersInputTxt.Text);
+
+            //Parameters will be null if the user entered invalid parameters as input
+            if(parameters == null)
+            {
+                //Show Output?
+                OutputFieldLbl.Text = "Please Enter the Correct Parameter types - space seperated";
+                return;
+            }
+
+            //Create an instance of the Type, then invoke the method on that instance of the Type
+            var instanceOfType = type.GetType().Assembly.CreateInstance(type.GetType().FullName);
+
+            object result = mi.GetMethodInfo().Invoke(instanceOfType, parameters);
+            OutputTxt.Text = result?.ToString();
+        }
+    }
+
+    class AssemblyInterpreter
+    {
+        public static readonly string TypeIdentifier = "Type: ";
+        public static readonly string MethodIdentifier = "Method: ";
+        public static readonly string ParameterIdentifier = "Parameter: ";
+
+        public IEnumerable<TypeInfo> GetDllInfo(string DllPath)
+        {
+            //Input Validation
+            if(string.IsNullOrWhiteSpace(DllPath) || !File.Exists(DllPath)) yield break;
+
+            Assembly LoadedAssembly = Assembly.LoadFrom(DllPath);
+            
+            //Loop Through all types in this assembly
+            foreach(var t in LoadedAssembly.GetTypes())
+            {
+                TypeInfo typeInfo = new TypeInfo(t);
+
+                //Loop through all the methods in this Type
+                foreach (var m in t.GetMethods())
+                    typeInfo.AddMethodInfo(m);
+
+                yield return typeInfo;
+            }
+        }
+        public TreeNodeInfo GetSelectedNodeInfo(TreeNode node)
+        {
+            TreeNodeInfo nodeInfo = new();
+
+            string nodeText = node.Text;
+            if(nodeText.Contains(TypeIdentifier))
+            {
+                nodeInfo.Category = TypeCategory.TYPE;
+
+                string typeName = GetTypeName(nodeText);
+                nodeInfo.TypeName = typeName;
+
+                //Can't do anything about a method because no method was selected
+                return nodeInfo;
+            }
+            else if(nodeText.Contains(MethodIdentifier))
+            {
+                nodeInfo.Category = TypeCategory.METHOD;
+
+                //Method Name Node
+                string methodName = GetMethodName(nodeText);
+
+                //Loop through the parameter nodes
+                string parameters = "";
+
+                //Only look for parameters if there are actually parameters shown in the TreeView
+                //Otherwise indexOutOfBounds Exceptions will occur
+                if (node.Nodes.Count > 0)
+                {
+                    for (int i = 0; i < node.Nodes.Count - 1; i++)
+                        parameters += GetParameterType(node.Nodes[i].Text) + " ";
+
+                    //Add the last parameter without a '#'
+                    parameters += GetParameterType(node.Nodes[node.Nodes.Count - 1].Text);
+                }
+
+                nodeInfo.TypeName = GetTypeName(node.Parent.Text);
+                nodeInfo.MethodName = methodName;
+                nodeInfo.Parameters = parameters;
+
+                return nodeInfo;
+            }
+            else if (nodeText.Contains(ParameterIdentifier))
+            {
+                nodeInfo.Category = TypeCategory.PARAMETER;
+                nodeInfo.Parameters = GetParameterType(nodeText);
+                return nodeInfo;
+            }
+            else throw new Exception("Invalid Text Inside Tree Node!");
+
+            return nodeInfo;
+        }
+        public (TypeInfo, DllMethodInfo) FindSelectedMethod(TreeNode methodNode, List<TypeInfo> typeInfoArr)
+        {
+            //'type' variable refers to the type that contains the method we are searching for
+            string typeName = GetTypeName(methodNode.Parent.Text);
+            TypeInfo type = typeInfoArr.Find(ti => ti.GetType().Name == typeName);
+
+            string methodName = GetMethodName(methodNode.Text);
+            DllMethodInfo method = type.GetMethodInfo().Find(mi => mi.Name == methodName);
+
+            return (type, method);
+        }
+        public object[] GetParameterValues(DllMethodInfo mi, string input)
+        {
+            //Input Validation
+            string[] inputs = input.Split(" ");
+            int paramCount = mi.ParameterInfo.Length;
+
+            //INVALID -- RETURN
+            if (paramCount != inputs.Length) return null;
+
+            //Compiling the parameters
+            List<object> parameters = new();
+            for (int i = 0; i < mi.ParameterInfo.Length; i++)
+            {
+                var paramInfo = mi.ParameterInfo[i];
+
+                //Finding out which type it is
+                if(Object.ReferenceEquals(paramInfo.ParameterType, typeof(string)))
+                {
+                    //Add a direct string to the parameter list
+                    parameters.Add(inputs[i]);
+                }
+                else if(Object.ReferenceEquals(paramInfo.ParameterType, typeof(int)))
+                {
+                    //Parse to int, then add to parameter list
+                    if (!int.TryParse(inputs[i], out var parsed)) return null;
+                    parameters.Add(parsed);
+                }
+                else if(Object.ReferenceEquals(paramInfo.ParameterType, typeof(bool)))
+                {
+                    //Parse to bool, then add to parameter list
+                    if (!bool.TryParse(inputs[i], out var parsed)) return null;
+                    parameters.Add(parsed);
+                }
+                else if (Object.ReferenceEquals(paramInfo.ParameterType, typeof(float)))
+                {
+                    //Parse to bool, then add to parameter list
+                    if (!float.TryParse(inputs[i], out var parsed)) return null;
+                    parameters.Add(parsed);
+                }
+                else if (Object.ReferenceEquals(paramInfo.ParameterType, typeof(double)))
+                {
+                    //Parse to bool, then add to parameter list
+                    if (!float.TryParse(inputs[i], out var parsed)) return null;
+                    parameters.Add(parsed);
+                }
+                else if (Object.ReferenceEquals(paramInfo.ParameterType, typeof(char)))
+                {
+                    //Parse to bool, then add to parameter list
+                    if (!char.TryParse(inputs[i], out var parsed)) return null;
+                    parameters.Add(parsed);
+                }
+            }
+            
+            return parameters.ToArray();
+        }
+
+        //String Manipulation Methods - Takes Text Directly from the TreeView
+        string GetTypeName(string _nodeText)
+                => _nodeText
+                .Split(TypeIdentifier)[1];
+        string GetMethodName(string _nodeText)
+            => _nodeText
+            .Split(MethodIdentifier)[1]
+            .Split(":")[0];
+        string GetParameterType(string _nodeText)
+            => _nodeText
+            .Split(ParameterIdentifier)[1]
+            .Split(":")[1]
+            .Split("=")[0];
+    }
+    struct TypeInfo
+    {
+        public TypeInfo(Type _t)
+        {
+            _type = _t;
+        }
+
+        private Type _type { get; set; }
+
+        //Stores Info on All the methods of the Set Type
+        private List<DllMethodInfo> _methodInfo { get; set; }
+            = new List<DllMethodInfo>();
+
+        public void AddMethodInfo(MethodInfo m)
+            => _methodInfo.Add(new DllMethodInfo(m));
+
+        //GETTERS
+        public Type GetType() => _type;
+        public List<DllMethodInfo> GetMethodInfo() => _methodInfo;
+    }
+    struct DllMethodInfo
+    {
+        string _name, _methodType;
+        MethodInfo _methodInfo;
+        ParameterInfo[] _parameterInfo;
+
+        public DllMethodInfo(MethodInfo m)
+        {
+            _name = m.Name;
+            _methodType = m.ReturnType.Name;
+            _methodInfo = m;
+            _parameterInfo = m.GetParameters();
+        }
+
+        public string Name => _name;
+        public string ReturnType => _methodType;
+        public MethodInfo GetMethodInfo() => _methodInfo;
+        public ParameterInfo[] ParameterInfo => _parameterInfo;
+    }
+    //Handles ALL infotmation that needs to be shown on the GUI when 
+    //the user selects a node on the Tree View
+    struct TreeNodeInfo
+    {
+        public TreeNodeInfo()
+        {
+            //Default it to TYPE
+            Category = TypeCategory.TYPE;
+        }
+
+        public TypeCategory Category;
+        public string TypeName = "", MethodName = "";
+        public string Parameters = "";
+    }
+    enum TypeCategory
+    {
+        TYPE = 0, METHOD = 1, PARAMETER = 2
     }
 }
